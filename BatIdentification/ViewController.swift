a72258fe4e9fbf863d26a69f4416b067a620e74c
Richard Beattie <richard.beattie@aol.co.uk>
2018-06-05 10:51:32 +0100

    Initial Commit

---
+++ BatIdentification/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BatIdentification
+//
+//  Created by Richard Beattie on 6/5/18.
+//  Copyright © 2018 Richard Beattie. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
